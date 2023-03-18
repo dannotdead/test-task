9572146839e9a8d29e73f110c93ecb73ebf298c9
dannotdead <daniil.koka2000@gmail.com>
2023-03-18 12:01:16 +0700

    finish first task

--- 1 task/index.js
+++ 1 task/index.js
@@ -1,24 +1,66 @@
-const inputEl = document.querySelector('input');
-const buttonEl = document.querySelector('button');
-const timerEl = document.querySelector('span');
+const inputEl = document.querySelector('input')
+const buttonEl = document.querySelector('button')
+const timerEl = document.querySelector('span')
 
-// Напишите реализацию createTimerAnimator
-// который будет анимировать timerEl
+const formatNumber = (number, length) => {
+  return number.toString().padStart(length, '0')
+}
+
+const formatTime = (time) => {
+  const hours = Math.floor(time / 3600)
+  const minutes = Math.floor((time % 3600) / 60)
+  const seconds = time % 60
+
+  return [
+    formatNumber(hours, 2),
+    formatNumber(minutes, 2),
+    formatNumber(seconds, 2),
+  ]
+}
+
+const formatTimerText = (seconds) => {
+  const [hours, minutes, secondsFormatted] = formatTime(seconds)
+
+  return `${hours}:${minutes}:${secondsFormatted}`
+}
+
 const createTimerAnimator = () => {
-  return (seconds) => {};
-};
+  let intervalId
 
-const animateTimer = createTimerAnimator();
+  return (seconds) => {
+    if (!seconds) {
+      return
+    }
+
+    clearInterval(intervalId)
+
+    timerEl.innerText = formatTimerText(seconds)
+
+    intervalId = setInterval(() => {
+      seconds -= 1
+
+      timerEl.innerText = formatTimerText(seconds)
+
+      if (seconds <= 0) {
+        clearInterval(intervalId)
+      }
+    }, 1000)
+  }
+}
+
+const animateTimer = createTimerAnimator()
 
 inputEl.addEventListener('input', () => {
-  // Очистите input так, чтобы в значении
-  // оставались только числа
-});
+  const currentValue = inputEl.value
+  const cleanedValue = currentValue.replace(/[^\d]/g, '')
+
+  inputEl.value = cleanedValue
+})
 
 buttonEl.addEventListener('click', () => {
-  const seconds = Number(inputEl.value);
+  const seconds = Number(inputEl.value)
 
-  animateTimer(seconds);
+  animateTimer(seconds)
 
-  inputEl.value = '';
-});
+  inputEl.value = ''
+})
