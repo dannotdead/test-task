0962a80a379f00228b2f25c058636643b63c35c3
dannotdead <daniil.koka2000@gmail.com>
2023-03-18 18:19:17 +0700

    init react project

---
+++ 2task/src/pages/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+      <header>logo nav div</header>
+
+      <main>section aside</main>
+
+      <footer></footer>
+    </>
+  )
+}
+
+export default Main
